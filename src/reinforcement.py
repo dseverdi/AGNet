478ec7c82040c07d5f1fc0c8b64b4f79a240a61d
dseverdi <dseverdi@gmail.com>
2025-01-30 11:46:15 +0100

    added entropy regularization

--- src/reinforcement.py
+++ src/reinforcement.py
@@ -17,6 +17,11 @@ import PtrNet
 def get_model_name_and_create_paths(args):
     0
 
+
+# weights
+gamma_1 = 0.8
+gamma_2 = 1-gamma_1
+
 from demo import evaluate_polygon_visibility_numpy_wo_gt, CustomError
 
 
@@ -50,7 +55,14 @@ def batch_eval_coverage(seq, seq_lens, pointer_indices):
     
     return coverages, relative_lengths, fine
 
-def evaluate_model(model, data_loader, device, writer, epoch_i, mode='Validation'):
+def evaluate_model(
+        model: PtrNet.PointerNetwork, 
+        data_loader : DataLoader, 
+        device : torch.device, 
+        writer : SummaryWriter, 
+        epoch_i : int, 
+        mode : str ='Validation'
+    ):
     model.eval()
     total_coverage = 0
     total_relative_length = 0
@@ -65,10 +77,7 @@ def evaluate_model(model, data_loader, device, writer, epoch_i, mode='Validation
             coverages, relative_lengths, fine = batch_eval_coverage(seq.cpu(), seq_lens, pointer_indices)
             coverages = coverages.to(device)
             relative_lengths = relative_lengths.to(device)
-            fine = fine.to(device)
-
-            gamma_1 = 0.5
-            gamma_2 = 0.5
+            fine = fine.to(device)            
 
             rewards = gamma_1 * (1 - coverages) + gamma_2 * relative_lengths
 
@@ -99,11 +108,14 @@ def train_model(
         writer : SummaryWriter, 
         num_epochs: int,
         use_critic: bool = False,
+        entropy_weight: float = 0.01  # Add entropy weight parameter
         ):
 
     max_grad_norm = 2
     best_coverage = 0
     best_epoch = 0
+
+    print(f"Using bello critic: {use_critic}")
 
     for epoch_i in range(num_epochs):
         critic_exp_mvg_avg = torch.zeros(1).to(device)
@@ -115,32 +127,34 @@ def train_model(
             # solution is sampled from the model
             pointer_indices, pointer_log_scores = model(seq, seq_lens)[0]
             
+            # gather coverage, relative length and fine signals
             coverages, relative_lengths, fine = batch_eval_coverage(seq.cpu(), seq_lens, pointer_indices)
+            # move to device
             coverages = coverages.to(device)
             relative_lengths = relative_lengths.to(device)
             fine = fine.to(device)
             
-            gamma_1 = 0.5
-            gamma_2 = 0.5
-            
-            rewards = gamma_1 * (1 - coverages) + gamma_2 * (1 - relative_lengths)
-            
-            beta = 0.9
-            
+                    
+            # define rewards
+            rewards = gamma_1 * (1 - coverages) + gamma_2 * relative_lengths
+                        
+            # 
             if batch_idx == 0:
                 critic_exp_mvg_avg = rewards.mean()
             else:    
-                critic_exp_mvg_avg = (critic_exp_mvg_avg * beta) + ((1. - beta) * rewards.mean())                
+                beta = 0.9
+                critic_exp_mvg_avg = (critic_exp_mvg_avg * beta) + ((1. - beta) * rewards.mean())
             
             if use_critic:
-                critic_bello_et_all = critic(seq, seq_lens)            
+                critic_bello_et_all = critic(seq, seq_lens)
+                rewards = rewards.detach()
                 advantage = rewards - critic_bello_et_all
             else:
-                advantage = rewards - critic_exp_mvg_avg            
-            
+                advantage = (rewards - critic_exp_mvg_avg).detach()
             
             batch_size = seq.size(1)
             log_probs = torch.empty(batch_size, dtype=advantage.dtype, device=device)
+            entropy = torch.empty(batch_size, dtype=advantage.dtype, device=device)  # Initialize entropy tensor
             for b_i in range(batch_size):
                 pointers_i = pointer_indices[:, b_i]
                 zero_indices = (pointers_i == 0).nonzero()
@@ -153,16 +167,22 @@ def train_model(
                 pointers_i_x = torch.arange(pointers_i.size(0))
                 log_probs_sum_i = pointer_log_scores[pointers_i_x, pointers_i_y, b_i].sum()
                 log_probs[b_i] = log_probs_sum_i
+                
+                # Calculate entropy
+                entropy[b_i] = -torch.sum(pointer_log_scores[pointers_i_x, pointers_i_y, b_i] * torch.exp(pointer_log_scores[pointers_i_x, pointers_i_y, b_i]))
             
             reinforce = torch.mean(advantage * log_probs)  # p. 5, Algorithm 1, line 9
             critic_loss = torch.mean(advantage ** 2)  # p. 5, Algorithm 1, line 9
+            entropy_loss = torch.mean(entropy)  # Calculate mean entropy
             
-            loss = reinforce + critic_loss
+            # minimize the negative of the loss
+            loss = reinforce + critic_loss if use_critic else reinforce
+            loss -= entropy_weight * entropy_loss  # Subtract entropy regularization term
             
             optimizer.zero_grad()
             
             loss.backward()
-            #torch.nn.utils.clip_grad_norm_(model.parameters(), float(max_grad_norm), norm_type=2)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm, norm_type=2)
 
             optimizer.step()
 
@@ -172,6 +192,7 @@ def train_model(
                 avg_coverage = coverages.mean().item()
                 avg_rel_length = relative_lengths.mean().item()
                 avg_reward = rewards.mean().item()
+                avg_entropy = entropy.mean().item()  # Calculate average entropy
                 #print(f"epoch: {epoch_i}, batch_idx: {batch_idx}, avg. coverage: {avg_coverage}, avg. rel. length: {avg_rel_length}, avg. reward: {avg_reward}")
 
                 # Log statistics to TensorBoard
@@ -179,6 +200,7 @@ def train_model(
                 writer.add_scalar('Train/Coverage', avg_coverage, epoch_i * len(training_generator) + batch_idx)
                 writer.add_scalar('Train/Relative Length', avg_rel_length, epoch_i * len(training_generator) + batch_idx)
                 writer.add_scalar('Train/Reward', avg_reward, epoch_i * len(training_generator) + batch_idx)
+                writer.add_scalar('Train/Entropy', avg_entropy, epoch_i * len(training_generator) + batch_idx)  # Log entropy
 
         # Evaluate the model on the validation set
         evaluate_model(model, validation_generator, device, writer, epoch_i, mode='Validation')
@@ -214,6 +236,7 @@ if __name__ == '__main__':
     parser.add_argument('--test-data', type=str, default='test', help='path to test samples')
     parser.add_argument('--sample-size', type=int, default=100, help='Number of samples to use')
     parser.add_argument('--use-critic', action='store_true', help='Use Bello critic instead of exp_mvg_avg')
+    parser.add_argument('--comment', type=str, default='', help='Additional comment for the model name')
     args = parser.parse_args()
 
     dataset_dir = "dataset/development"
@@ -222,7 +245,7 @@ if __name__ == '__main__':
     valid_path = os.path.join(dataset_dir,f'{args.valid_data}')
     test_path = os.path.join(dataset_dir,f'{args.test_data}')
     
-    model_name = f'ne-{args.num_epochs}_bs-{args.batch_size}_hs-{args.hidden_size}_hv-{args.hidden_v}_wd-{args.wd}_lr-{args.lr}_ss-{args.sample_size}'
+    model_name = f'ne-{args.num_epochs}_bs-{args.batch_size}_hs-{args.hidden_size}_hv-{args.hidden_v}_wd-{args.wd}_lr-{args.lr}_ss-{args.sample_size}_wt_cov-{gamma_1}'
     if args.use_critic:
         model_name += '_critic'
     if args.bidirectional:
@@ -230,6 +253,9 @@ if __name__ == '__main__':
     
     if args.normalize:
         model_name += '_normalized'
+
+    if args.comment:
+        model_name += f'_{args.comment}'
 
     model_path = f'./trained_models/{model_name}/'
     if not os.path.exists(model_path):
@@ -269,7 +295,7 @@ if __name__ == '__main__':
     model = PtrNet.PointerNetwork(model_args).to(device)
     critic = PtrNet.Critic(model_args).to(device)
     #optimizer = optim.AdamW(model.parameters(), lr=args.lr, weight_decay=args.wd)    
-    optimizer = optim.Adam([ {"params": model.parameters()}, { "params": critic.parameters() }], lr=1e-4)
+    optimizer = optim.Adam([ {"params": model.parameters()}, { "params": critic.parameters() }], lr=1e-3)
     #optimizer_critic = optim.Adam(critic.parameters(), lr=1e-4)
 
     # Initialize TensorBoard writer
